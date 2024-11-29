1fa61e4b1017ceacd2566e68524c6c119cb6d712
Ujjwal Kumar <ujjwalkr80@gmail.com>
2024-11-29 21:58:11 +0530

    added site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  site: "https://ujjwal313.github.io",
+  base: "Elia-Utsav-NGO",
+});
