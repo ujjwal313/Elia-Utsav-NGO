e9deb8b799962e936e3f5a0bd28718db2bb21720
Ujjwal Kumar <ujjwalkr80@gmail.com>
2024-12-01 10:38:49 +0530

    fix: deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://ujjwal313.github.io",
+  base: "Elia-Utsav-NGO",
 });
